934673e3832922eeef1ebca32d880c1e0117d409
PronkoEvgeniya <94559003+PronkoEvgeniya@users.noreply.github.com>
2022-03-01 13:16:44 +0300

    feat: add files

---
+++ audio-player/list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+    {
+        name: "Shock",
+        artist: "Yuko Ando",
+        img: "falko",
+        src: "shock",
+    },
+    {
+        name: "Again",
+        artist: "Yui",
+        img: "edvard",
+        src: "again",
+    },
+    {
+        name: "Gurenge",
+        artist: "LiSa",
+        img: "demon",
+        src: "Gurenge",
+    },
+    {
+        name: "YOUSEEBIGGIRL",
+        artist: "Hiroyuki Sawano",
+        img: "eren",
+        src: "YOUSEEBIGGIRL",
+    },
+    {
+        name: "Closer",
+        artist: "Joe Inoue",
+        img: "shika",
+        src: "closer",
+    },
+    {
+        name: "Cry Baby",
+        artist: "HIGE DANDISM",
+        img: "tokyorev",
+        src: "cry-baby",
+    }
+]
+
+export default allMusic;
